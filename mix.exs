5701d5b2b4437eb8d8676b5fbab7a8104d35dfd4
Ilkka Poutanen <ilkka@ilkka.dev>
2023-09-13 16:55:03 +0300

    Rename some mix aliases

--- mix.exs
+++ mix.exs
@@ -65,10 +65,10 @@ defmodule Sankolista.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      setup: ["deps.get", "database.setup", "assets.setup", "assets.build"],
+      "database.setup": ["ash_postgres.create", "ash_postgres.migrate", "run priv/repo/seeds.exs"],
+      "database.reset": ["ash_postgres.drop", "database.setup"],
+      test: ["ash_postgres.create --quiet", "ash_postgres.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
