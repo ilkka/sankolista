cf07db1877c4a3cae43160e970b6d543078f8f2b
Ilkka Poutanen <ilkka@ilkka.dev>
2023-09-18 16:56:02 +0300

    Vapor based runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -7,6 +7,24 @@ import Config
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
 
+values = Vapor.load!([
+  %Vapor.Provider.Dotenv{overwrite: true},
+  %Vapor.Provider.Env{
+    bindings: [
+      {:phx_server, "PHX_SERVER", default: false, map: fn s -> s in ~w(true 1) end},
+      {:hostname, "PHX_HOST", default: "localhost"},
+      {:port, "PORT", default: 4000, map: &String.to_integer/1},
+      {:url_scheme, "URL_SCHEME", default: "http", }
+      {:secret_key_base, "SECRET_KEY_BASE"},
+      {:auth0_client_id, "AUTH0_CLIENT_ID"},
+      {:auth0_client_secret, "AUTH0_CLIENT_SECRET"},
+      {:database_url, "DATABASE_URL"},
+      {:pool_size, "POOL_SIZE", default: 10, map: &String.to_integer/1},
+      {:ecto_ipv6, "ECTO_IPV6", default: false, map: fn s -> s in ~w(true 1) end}
+    ]
+  }
+])
+
 # ## Using releases
 #
 # If you use `mix release`, you need to explicitly enable the server
@@ -16,29 +34,23 @@ import Config
 #
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
-if System.get_env("PHX_SERVER") do
+if values.phx_server do
   config :sankolista, SankolistaWeb.Endpoint, server: true
 end
 
 config :sankolista, :auth0,
-  client_id: System.get_env("AUTH0_CLIENT_ID") || raise("environment variable AUTH0_CLIENT_ID is missing"),
-  client_secret: System.get_env("AUTH0_CLIENT_SECRET") || raise("environment variable AUTH0_CLIENT_SECRET is missing")
+  client_id: values.client_id,
+  client_secret: values.client_secret
 
+config :sankolista, Sankolista.Repo,
+  url: values.database_url,
+  pool_size: values.pool_size
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
-
-  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
+  maybe_ipv6 = if values.ecto_ipv6, do: [:inet6], else: []
 
   config :sankolista, Sankolista.Repo,
     # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
@@ -46,18 +58,8 @@ if config_env() == :prod do
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
-
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
-
   config :sankolista, SankolistaWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: values.hostname, port: values.port, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
