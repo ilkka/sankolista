2bc59cb964e227dcc1ac9557f667e67e23e87da9
Ilkka Poutanen <ilkka@ilkka.dev>
2023-09-18 16:55:35 +0300

    Impl tags for secrets

--- lib/sankolista/secrets.ex
+++ lib/sankolista/secrets.ex
@@ -1,10 +1,12 @@
 defmodule Sankolista.Secrets do
   use AshAuthentication.Secret
 
+  @impl AshAuthentication.Secret
   def secret_for([:authentication, :strategies, :auth0, :client_id], Sankolista.Accounts.User, _) do
     get_config(:client_id)
   end
 
+  @impl AshAuthentication.Secret
   def secret_for(
         [:authentication, :strategies, :auth0, :client_secret],
         Sankolista.Accounts.User,
@@ -13,6 +15,7 @@ defmodule Sankolista.Secrets do
     get_config(:client_secret)
   end
 
+  @impl AshAuthentication.Secret
   def secret_for(
         [:authentication, :strategies, :auth0, :redirect_uri],
         Sankolista.Accounts.User,
@@ -21,6 +24,7 @@ defmodule Sankolista.Secrets do
     get_config(:redirect_uri)
   end
 
+  @impl AshAuthentication.Secret
   def secret_for([:authentication, :strategies, :auth0, :site], Sankolista.Accounts.User, _) do
     get_config(:site)
   end
