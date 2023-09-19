604e04fbc52c84b94904e61ec4ee21c255aa7250
Ilkka Poutanen <ilkka@ilkka.dev>
2023-09-19 15:48:04 +0300

    Fix more refs to valuesa

--- config/runtime.exs
+++ config/runtime.exs
@@ -66,9 +66,9 @@ if config_env() == :prod do
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: values.port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: values.secret_key_base
 
   # ## SSL Support
   #
